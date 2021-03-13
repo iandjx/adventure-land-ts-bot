49df69483197ac7e9eaa3f02ef915538b650d83d
Ian de Jesus <ian@eskwelabs.com>
2021-03-13 20:13:52 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
-import { setupParty } from './global/party';
-import * as AL from 'alclient';
-import path from 'path';
+import {setupParty} from './global/party'
+import * as AL from 'alclient'
+import path from 'path'
 
+const sum = a => {
+  console.log(a)
+}
 async function run() {
   await Promise.all([
     AL.Game.loginJSONFile(path.resolve('credentials.json')),
     AL.Pathfinder.prepare(),
-  ]);
-  const merchant = await AL.Game.startMerchant('Merchiex', 'ASIA', 'I');
-  const ranger = await AL.Game.startRanger('crosscutting', 'ASIA', 'I');
-  const priest = await AL.Game.startPriest('priestiex', 'ASIA', 'I');
-  const warrior = await AL.Game.startWarrior('rastarix', 'ASIA', 'I');
+  ])
+  const merchant = await AL.Game.startMerchant('Merchiex', 'ASIA', 'I')
+  const ranger = await AL.Game.startRanger('crosscutting', 'ASIA', 'I')
+  const priest = await AL.Game.startPriest('priestiex', 'ASIA', 'I')
+  const warrior = await AL.Game.startWarrior('rastarix', 'ASIA', 'I')
 
-  const farmers = [ranger, priest, warrior];
+  const farmers = [ranger, priest, warrior]
 
-  await setupParty(merchant, farmers);
+  await setupParty(merchant, farmers)
 
-  await merchant.smartMove('fancypots');
-  await merchant.buy('hpot0', 1);
+  await merchant.smartMove('fancypots')
+  await merchant.buy('hpot0', 1)
 
-  AL.Game.disconnect();
+  AL.Game.disconnect()
 }
-run();
+run()
