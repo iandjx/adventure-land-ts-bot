e6b4bb26b0880ffd11db15c91818857a065e8194
Ian de Jesus <ian@eskwelabs.com>
2021-03-13 20:02:31 +0900

    updated static testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,26 @@
 // .eslintrc.js
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es6: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
   },
-  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
-  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'eslint:recommended',
-    'prettier/@typescript-eslint', // Prettier plugin
-    'plugin:prettier/recommended', // Prettier recommended rules
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'eslint-config-prettier/@typescript-eslint',
   ],
-  overrides: [
-    // This configuration will apply only to TypeScript files
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      settings: { react: { version: 'detect' } },
-      env: {
-        browser: true,
-        node: true,
-        es6: true,
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended', // TypeScript rules
-        'plugin:react/recommended', // React rules
-        'plugin:react-hooks/recommended', // React hooks rules
-        'plugin:jsx-a11y/recommended', // Accessibility rules
-      ],
-      rules: {
-        // We will use TypeScript's types for component props instead
-        'react/prop-types': 'off',
-
-        // No need to import React when using Next.js
-        'react/react-in-jsx-scope': 'off',
-
-        // This rule is not compatible with Next.js's <Link /> components
-        'jsx-a11y/anchor-is-valid': 'off',
-
-        // Why would you want unused vars?
-        '@typescript-eslint/no-unused-vars': ['error'],
-
-        // I suggest this setting for requiring return types on functions only where useful
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-          },
-        ],
-
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
-      },
-    },
-  ],
-};
+  rules: {
+    strict: ['error', 'never'],
+  },
+  env: {
+    browser: true,
+  },
+}
