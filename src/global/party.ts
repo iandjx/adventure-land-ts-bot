f4ae41b79a106c94803526d6a722bc3531c23828
Ian de Jesus <ian@eskwelabs.com>
2021-03-13 21:07:16 +0900

    fixed types

--- src/global/party.ts
+++ src/global/party.ts
@@ -1,13 +1,16 @@
-import { Character, Merchant } from 'alclient';
-import { timeout } from 'utils/set-timeout';
+import {Character, Merchant} from 'alclient'
+import {timeout} from 'utils/set-timeout'
 
-export const setupParty = async (merchant: Merchant, farmers: Character[]) => {
+export const setupParty = async (
+  merchant: Merchant,
+  farmers: Character[],
+): Promise<void> => {
   for (const farmer of farmers) {
     while (farmer.party === undefined) {
-      console.log('creating party');
-      merchant.sendPartyInvite(farmer.id);
-      await timeout(100);
-      farmer.acceptPartyInvite(merchant.id);
+      console.log('creating party')
+      merchant.sendPartyInvite(farmer.id)
+      await timeout(100)
+      farmer.acceptPartyInvite(merchant.id)
     }
   }
-};
+}
--- src/utils/set-timeout.ts
+++ src/utils/set-timeout.ts
@@ -1,3 +1,3 @@
-export const timeout = (ms: number | undefined) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
+export const timeout = (ms: number | undefined): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
