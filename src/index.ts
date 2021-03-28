99d9229f9d330456cdea29154af17043a4e4ce81
Ian de Jesus <ian@eskwelabs.com>
2021-03-28 10:56:06 +0900

    add send loot fn

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,14 @@ async function run() {
     AL.Game.loginJSONFile(path.resolve('credentials.json')),
     AL.Pathfinder.prepare(),
   ])
-  const warrior = await AL.Game.startWarrior('rastarix', 'ASIA', 'I')
-  const merchant = await AL.Game.startMerchant('Merchiex', 'ASIA', 'I')
-  const ranger = await AL.Game.startRanger('crosscutting', 'ASIA', 'I')
-  const priest = await AL.Game.startPriest('priestiex', 'ASIA', 'I')
+  const warrior = await AL.Game.startWarrior('rastarix', 'US', 'II')
+  const merchant = await AL.Game.startMerchant('Merchiex', 'US', 'II')
+  const ranger = await AL.Game.startRanger('crosscutting', 'US', 'II')
+  const priest = await AL.Game.startPriest('priestiex', 'US', 'II')
 
   const farmers = [ranger, priest, warrior]
-
+  let healing = false
+  let farmerToSendGoldFrom = ''
   setupParty(merchant, farmers)
   console.log('Adventure Start!')
 
@@ -46,11 +47,18 @@ async function run() {
         try {
           if (member.hp <= member.max_hp - maxHeal) {
             console.log(member.id, ' needs healing. current hp ', member.hp)
-            console.log(priest.canUse('heal'), 'can heal?')
-            // if (priest.canUse('heal')) {
-            console.log('healling')
+
+            if (getDistance(priest, member) > priest.range) {
+              await priest.smartMove({x: member.x, y: member.y})
+              healing = true
+              continue
+            }
+            const cooldown = priest.getCooldown('attack')
+            if (cooldown > 0) await sleep(cooldown) // Wait for attack to become ready
+
             await priest.heal(member.id).catch(err => console.log(err))
-            // }
+            console.log(member.id, ' healed for ', maxHeal)
+            healing = false
           }
         } catch (e) {
           console.error(e)
@@ -60,7 +68,7 @@ async function run() {
       await sleep(2000) /* Wait a bit until the next regen */
     }
   }
-  healLoop(priest, [ranger, warrior])
+  healLoop(priest, [merchant, ranger, warrior])
 
   async function regenLoop() {
     while (true) {
@@ -99,6 +107,15 @@ async function run() {
     while (true) {
       for (const farmer of farmers) {
         try {
+          if (healing === true) {
+            if (farmer instanceof Priest) {
+              continue
+            }
+          }
+
+          if (farmer.id === farmerToSendGoldFrom) {
+            continue
+          }
           // console.log('Finding nearest crab to move to')
           const nearestGoo = farmer.getNearestMonster('crab')
           if (!nearestGoo) {
@@ -127,14 +144,19 @@ async function run() {
     while (true) {
       for (const farmer of farmers) {
         try {
+          if (farmer instanceof Priest && healing) {
+            continue
+          }
+          if (farmer.id === farmerToSendGoldFrom) {
+            continue
+          }
+
           const cooldown = farmer.getCooldown('attack')
           if (cooldown > 0) await sleep(cooldown) // Wait for attack to become ready
 
           if (farmer.canUse('attack')) {
             const attackableGoo = farmer.getNearestMonster('crab')
             if (attackableGoo && attackableGoo.distance < farmer.range) {
-              // We're close enough to attack
-              console.log(farmer.id, 'attacking', attackableGoo.monster.id)
               await farmer.basicAttack(attackableGoo.monster.id).catch(() => {
                 /* Empty to suppress messages */
               })
@@ -149,5 +171,103 @@ async function run() {
     }
   }
   attackLoop()
+
+  async function chestLoop() {
+    while (true) {
+      for (const farmer of farmers) {
+        try {
+          farmer.chests.forEach(async chest => {
+            await farmer.openChest(chest.id)
+          })
+        } catch (e) {
+          // console.error(e)
+        }
+      }
+
+      await sleep(1000) /* Wait a bit until the next attack */
+    }
+  }
+  chestLoop()
+
+  async function respawnLoop() {
+    while (true) {
+      for (const farmer of farmers) {
+        try {
+          if (merchant.rip) {
+            merchant.respawn()
+          }
+          if (farmer.rip) {
+            console.log(farmer.id, ' died respawning')
+            farmer.respawn()
+          }
+        } catch (e) {
+          // console.error(e)
+        }
+      }
+
+      await sleep(1000) /* Wait a bit until the next attack */
+    }
+  }
+  respawnLoop()
+
+  async function sendGold() {
+    while (true) {
+      for (const farmer of farmers) {
+        try {
+          while (farmer.gold >= 10000) {
+            farmerToSendGoldFrom = farmer.id
+
+            if (farmer.map !== merchant.map) {
+              if (!merchant.moving) {
+                console.log('going to farmer location')
+                merchant.smartMove(farmer.map)
+              }
+              continue
+            }
+
+            if (getDistance(merchant, farmer) > merchant.range) {
+              if (!merchant.moving) {
+                await merchant
+                  .smartMove({map: farmer.map, x: farmer.x, y: farmer.y})
+                  .catch(err => {
+                    console.log(err)
+                  })
+              }
+
+              continue
+            }
+            if (merchant.moving) {
+              continue
+            }
+
+            await farmer.sendGold(merchant.id, farmer.gold)
+            console.log(farmer.gold, ' given to', merchant.id)
+            farmerToSendGoldFrom = ''
+          }
+        } catch (err) {
+          console.log(err)
+        }
+      }
+      await sleep(250) /* Wait a bit until the next attack */
+    }
+  }
+  sendGold()
 }
 run()
+
+const getDistance = (characterA: Character, characterB: Character) => {
+  return distance_to_point(
+    characterA.x,
+    characterA.y,
+    characterB.x,
+    characterB.y,
+  )
+}
+
+// we take the x and y coordinates of a point, and compare it to another point
+// we can then derive the distance between two points
+function distance_to_point(x1: number, y1: number, x2: number, y2: number) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+}
