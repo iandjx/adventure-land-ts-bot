4b422c76a38eeff715303ed25df0badffd8ddad3
Ian de Jesus <ian@eskwelabs.com>
2021-04-02 15:37:17 +0900

    feat: add todo for cleave

---
+++ src/warrior/cleave.ts
@@ -0,0 +1,5 @@
+//TODO get x y location of monster group
+// TODO find midpoint
+//find distance between mid point and one point
+// create a circle at midpoint of the distane with radius 80
+//make ircle go around
