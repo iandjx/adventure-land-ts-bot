9109b5975613cf7cedd0585bc7446df4bf5255b5
Ian de Jesus <ian@eskwelabs.com>
2021-04-29 13:18:39 +0900

    feat: add item equip script

---
+++ src/global/itemEquip.ts
@@ -0,0 +1,13 @@
+// const warblade = warrior.items.findIndex(
+//   item => item.name === 'blade' && item.level === 4,
+// )
+// await warrior.equip(warblade)
+// const warbow = ranger.items.findIndex(
+//   item => item.name === 'bow' && item.level === 4,
+// )
+// await ranger.equip(warbow)
+
+// const warstaff = priest.items.findIndex(
+//   item => item.name === 'staff' && item.level === 4,
+// )
+// await priest.equip(warstaff)
