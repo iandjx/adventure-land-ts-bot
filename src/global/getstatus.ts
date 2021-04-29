42438ba0cf720d25bdbbc9dca6c4f05c42c4cc30
Ian de Jesus <ian@eskwelabs.com>
2021-04-29 13:14:09 +0900

    feat: update get status script

--- src/global/getstatus.ts
+++ src/global/getstatus.ts
@@ -1,16 +1,14 @@
-import {Character} from 'alclient'
+import { Character } from 'alclient'
 import sleep from '../utils/sleep'
 
 async function getStatus(farmers: Character[]): Promise<void> {
   while (true) {
-    try {
-      for (const farmer of farmers) {
-        console.log(farmer.id, ' status ')
-        console.log('Max HP ', farmer.max_hp, 'Current HP', farmer.hp)
-        console.log('ccurent exp', farmer.xp)
-      }
-    } catch (error) {
-      console.log(error)
+    for (const farmer of farmers) {
+      console.log(farmer.id, ' status ')
+      console.log('Max HP ', farmer.max_hp, 'Current HP', farmer.hp)
+      console.log('Current Exp', farmer.xp)
+      console.log('Current Level', farmer.level)
+      console.log('huntiung', farmer.s.monsterhunt)
     }
 
     await sleep(10000)
