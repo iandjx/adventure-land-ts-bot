045bd40ac16ba7b5e013b476531d3f5f8e63e43c
Ian de Jesus <ian@eskwelabs.com>
2021-04-29 17:38:17 +0900

    feat: added cooptargetmonster class prop

--- src/global/gameState.ts
+++ src/global/gameState.ts
@@ -1,10 +1,12 @@
-import {Character} from 'alclient'
+import { Character } from 'alclient'
+import { Entity } from 'alclient/build/Entity'
 
 type HealState = 'not-healing' | 'healing'
 
 export default class GameState {
   healState?: HealState
   goldSender?: Character | null
+  coopTargetMonster?: { monster: Entity; distance: number } | null
 
   set currentGoldSender(character: Character | null) {
     this.goldSender = character
